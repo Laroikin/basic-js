db5f2d460be244294b44d459a0143f7bea7e93fa
Laroikin <said24abdi04@gmail.com>
2022-04-29 02:04:25 +0500

    solved what-season

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+for (let i in seasons) {
+  //   if(string.toLowerCase() == seasons[i]){
+  //     return i;
+  //   }
+  // }
--- src/what-season.js
+++ src/what-season.js
@@ -2,19 +2,56 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function switchSeason(string) {
+  console.log(string);
+
+  string = string.split(' ')[1].toLowerCase();
+
+  seasons = {
+    winter: ['dec', 'jan', 'feb'],
+    summer: ['jun', 'jul', 'aug'],
+    spring: ['mar', 'apr', 'may'],
+    fall: ['sep', 'oct', 'nov']
+  };
+
+  ans = '';
+
+  for (let i in seasons) {
+    seasons[i].forEach(e => {
+      if (e == string) {
+        ans = i;
+      }
+    });
+  }
+
+  return ans;
 }
+
+function getSeason(date) {
+  if (arguments.length < 1) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (
+    Object.getOwnPropertyNames(date).length !== 0 ||
+    !(date instanceof Date && !isNaN(date))
+  )
+    throw Error('Invalid date!');
+
+  return switchSeason(date.toDateString());
+}
+
+// console.log(getSeason('foo'));
 
 module.exports = {
   getSeason
